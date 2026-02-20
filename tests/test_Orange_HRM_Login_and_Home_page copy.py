de53672da381bf7bfbb77705545dd12f80aa8a4f
Vijaya Bhaskar Reddy <vijayhmsv93@gmail.com>
2026-02-20 19:14:25 +0530

    updated copy file

--- tests/test_Orange_HRM_Login_and_Home_page copy.py
+++ tests/test_Orange_HRM_Login_and_Home_page copy.py
@@ -1,5 +1,19 @@
 import re
 from playwright.sync_api import Page, sync_playwright, expect
+from pages.OrageHRM_Home_page import homePage
+from pages.orangeHRM_login_page import loginPage
+
+def test_example(page:Page)->None:
+    
+    login_page =loginPage(page)
+    home_page=homePage(page)
+    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    login_page.enter_username("Admin")
+    login_page.enter_password("admin123")
+    login_page.click_login()
+
+    home_page.click_leave_menu()
+    home_page.logout()
 
 
 
