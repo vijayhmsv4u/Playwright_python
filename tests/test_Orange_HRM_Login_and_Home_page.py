687d1fad29372391e773f82afe1fbdb6bfcb3be0
Vijaya Bhaskar Reddy <vijayhmsv93@gmail.com>
2026-03-06 19:16:58 +0530

    update_code

--- tests/test_Orange_HRM_Login_and_Home_page.py
+++ tests/test_Orange_HRM_Login_and_Home_page.py
@@ -1,5 +1,5 @@
 import re
-from playwright.sync_api import Page, sync_playwright, expect
+from playwright.sync_api import sync_playwright,expect
 
 
 def test_exmaplerun(page) -> None:
@@ -10,13 +10,16 @@ def test_exmaplerun(page) -> None:
     page.get_by_role("textbox", name="Password").click()
     page.get_by_role("textbox", name="Password").fill("admin123")
     page.get_by_role("button", name="Login").click()
-    page.wait_for_timeout(5000)
+    page.wait_for_timeout(15000)
     expect(page.get_by_role("heading")).to_contain_text("Dashboard")
     page.get_by_role("link", name="Leave").click()
+    expect(page.locator("h6")).to_be_visible()
     expect(page.locator("h6")).to_contain_text("Leave")
     page.get_by_role("link", name="Performance").click()
+    expect(page.get_by_role("banner")).to_be_visible()
     expect(page.get_by_role("banner")).to_contain_text("Performance")
     page.get_by_role("banner").get_by_role("img", name="profile picture").click()
+    expect(page.get_by_role("menuitem", name="Logout")).to_be_visible()
     page.get_by_role("menuitem", name="Logout").click()
     expect(page.get_by_role("heading")).to_contain_text("Login")
 
